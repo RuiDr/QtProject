741b4f581553604f3b9b70bd8bd9961374fed3ef
RuiDr <291845444@qq.com>
2021-05-13 17:16:59 +0800

    翻金币案例

---
+++ CoinFlip/mainscene.h
@@ -0,0 +1,23 @@
+﻿#ifndef MAINSCENCE_H
+#define MAINSCENCE_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainScence;
+}
+
+class MainScence : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainScence(QWidget *parent = 0);
+    ~MainScence();
+
+private:
+    Ui::MainScence *ui;
+    void paintEvent(QPaintEvent *event);
+};
+
+#endif // MAINSCENCE_H
